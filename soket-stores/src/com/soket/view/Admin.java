0ff3b53d3b3ba409b45da00b6705fd4083117af1
Zeus Ogarrio <zeus.ogarrio@gmail.com>
2022-06-16 15:50:04 -0500

    starting new java project

---
+++ soket-stores/src/com/soket/view/Admin.java
@@ -0,0 +1,56 @@
+package com.soket.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.UIManager;
+import javax.swing.JList;
+import javax.swing.JRadioButtonMenuItem;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JSpinner;
+
+public class Admin extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Admin frame = new Admin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Admin() {
+		setTitle("Consola de Administraci\u00F3n");
+		try{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch(Exception e){
+		    System.out.println(e);
+		}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+	}
+}
